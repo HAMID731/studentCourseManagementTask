4ddb4082d2f6a3d986d3988494f52a04396db2ed
HAMID <abdulhamidabari652@gmail.com>
2025-03-05 14:42:02 +0100

    static

--- src/student.py
+++ src/student.py
@@ -59,7 +59,11 @@ class Student(Teacher):
 
         return enrolled_courses
 
-
+    @staticmethod
+    def verify_email(email):
+        if email in Student.emails:
+            raise EmailAlreadyExistException("Email already registered")
+        Student.emails.add(email)
 
     @staticmethod
     def verify_name(name):
